9e9e6b87f7d4e314ab5194a75595521feeda74a3
Tom Vaidyan <tvaidyan@gmail.com>
2023-01-22 17:28:45 -0600

    scaffold out post

--- src/Feed.js
+++ src/Feed.js
@@ -6,6 +6,7 @@ import ImageIcon from '@mui/icons-material/Image'
 import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
 import EventNoteIcon from '@mui/icons-material/EventNote'
 import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay'
+import Post from './Post'
 
 function Feed() {
   return (
@@ -25,6 +26,7 @@ function Feed() {
                 <InputOption Icon={CalendarViewDayIcon} title='Write article' color='#7FC15E' />
             </div>
         </div>
+        <Post name='Tom Vaidyan' description='This is a test.' message='WOW this worked' />
     </div>
   )
 }
---
+++ src/Post.js
@@ -0,0 +1,22 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+import './Post.css'
+
+function Post({ name, description, message, photoUrl }) {
+  return (
+    <div className='post'>
+        <div className="post__header">
+            <Avatar />
+            <div className="post__info">
+                <h2>Tom Vaidyan</h2>
+                <p>Description</p>
+            </div>
+        </div>
+        <div className='post__body'>
+            <p>Message goes here</p>
+        </div>
+    </div>
+  )
+}
+
+export default Post
