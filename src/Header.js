16989fb532ad109ce03ecbb8151231f03e98cbc0
Tom Vaidyan <tvaidyan@gmail.com>
2023-01-22 11:49:38 -0600

    clean up initial template, add header UI

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Header.css'
+import HomeIcon from "@mui/icons-material/Home"
+import SearchIcon from '@mui/icons-material/Search'
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'
+import HeaderOption from './HeaderOption'
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter'
+import ChatIcon from '@mui/icons-material/Chat'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className="header__left">
+          <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="" />
+          <div className="header__search">
+            <SearchIcon />
+            <input type="text" />
+          </div>
+        </div>
+        <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title='Home' />
+          <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+          <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+          <HeaderOption Icon={ChatIcon} title='Messaging' />
+          <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+        </div>
+    </div>
+  )
+}
+
+export default Header
